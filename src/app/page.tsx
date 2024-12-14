eb7ac67fc1dac48b3626a33a708cc7651c109f3a
shashankyadahalli <shashankyadahalli@gmail.com>
2024-12-14 07:32:43 +0000

    New code Added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,12 @@
-import IconCloud from "@/components/ui/icon-cloud";
- 
-const slugs = [
-  "typescript",
-  "javascript",
-  "dart",
-  "java",
-  "react",
-  "flutter",
-  "android",
-  "html5",
-  "css3",
-  "nodedotjs",
-  "express",
-  "nextdotjs",
-  "prisma",
-  "amazonaws",
-  "postgresql",
-  "firebase",
-  "nginx",
-  "vercel",
-  "testinglibrary",
-  "jest",
-  "cypress",
-  "docker",
-  "git",
-  "jira",
-  "github",
-  "gitlab",
-  "visualstudiocode",
-  "androidstudio",
-  "sonarqube",
-  "figma",
-];
+import GlobeDemo from "./Frontend/Earth";
+
+
+
 
 export default function Home() {
   return (
             <div className="relative flex size-full max-w-lg items-center justify-center overflow-hidden rounded-lg border bg-background px-20 pb-20 pt-8 ">
-            <IconCloud iconSlugs={slugs} />
+            <GlobeDemo/>
           </div>
   );
 }
