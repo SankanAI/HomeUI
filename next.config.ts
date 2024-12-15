87db71accfbb49b620bc05f54482c45363f441c4
shashankyadahalli <shashankyadahalli@gmail.com>
2024-12-15 04:51:21 +0000

    Pushed the Book

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",  // <=== enables static exports
-  reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true, // Ignores ESLint errors and warnings during the build process
   },
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/HomeUI/' : '',
+  basePath: isProd ? '/HomeUI' : '',+
 };
 
 export default nextConfig;
