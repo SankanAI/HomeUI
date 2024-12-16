275ae3b0846cf0b8c11bd2da9819c918b0be39f7
shashankyadahalli <shashankyadahalli@gmail.com>
2024-12-16 06:29:51 +0000

    Pushing New Design

---
+++ src/app/Frontend/Animated.tsx
@@ -0,0 +1,32 @@
+"use client";
+import React from "react";
+import { ContainerScroll } from "@/components/ui/container-scroll-animation";
+import Image from "next/image";
+
+export function HeroScrollDemo() {
+  return (
+    <div className="flex flex-col overflow-hidden">
+      <ContainerScroll
+        titleComponent={
+          <>
+            <h1 className="tracking-tighter text-4xl font-semibold text-white">
+              What is <br />
+              <span className="text-4xl md:text-[6rem] font-bold mt-1 leading-none">
+                Additional ?
+              </span>
+            </h1>
+          </>
+        }
+      >
+        <Image
+          src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNO42aYVnaS5phudCpBUkNsbfFbmvCEtb8kQ&s"}
+          alt="hero"
+          height={720}
+          width={1400}
+          className="mx-auto rounded-2xl object-cover h-full object-left-top"
+          draggable={false}
+        />
+      </ContainerScroll>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 
-import HeroParallaxDemo from "@/app/Frontend/Move"
-import AnimatedTooltipPreview from "./Frontend/AnimatedTool";
+import HeroParallaxDemo from "@/app/Frontend/Move";
+import { HeroScrollDemo } from "./Frontend/Animated";
+
 export default function Home() {
   return (
       <>
         <HeroParallaxDemo/>
-        <h1 className="w-full text-center text-5xl mt-5 tracking-tighter py-10">What is Additional ?</h1>
-        <AnimatedTooltipPreview/>
+        <HeroScrollDemo/>
       </>
   );
 }
