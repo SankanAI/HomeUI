79243dbbbc5cc63e8d3c2557becdf8e0fb62488c
shashankyadahalli <shashankyadahalli@gmail.com>
2024-12-16 05:26:20 +0000

    Pushing New Design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 
 import HeroParallaxDemo from "@/app/Frontend/Move"
+import AnimatedTooltipPreview from "./Frontend/AnimatedTool";
 export default function Home() {
   return (
-  <HeroParallaxDemo/>
+      <>
+        <HeroParallaxDemo/>
+        <h1 className="w-full text-center text-5xl mt-5 tracking-tighter py-10">What is Additional ?</h1>
+        <AnimatedTooltipPreview/>
+      </>
   );
 }
